a11f9f44bf0759d1d1d7aa28e7cd697be39b34c7
Richard Smith <richardsmith.digitaldwp@gmail.com>
2020-01-24 10:58:22 +0000

    Update to config.js adding Future to service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Health Assessment',
+  serviceName: 'Future Health Assessment',
 
   // Default port that prototype runs on
   port: '3000',
