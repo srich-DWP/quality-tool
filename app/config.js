2fbc495e87c5c4acb931cdc4d297a8c7606dce74
Richard Smith <richardsmith.digitaldwp@gmail.com>
2020-03-05 12:08:52 +0000

    Updating styles and remving Future from the headers. Health Assessment

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Future Health Assessment',
+  serviceName: 'Health Assessment',
 
   // Default port that prototype runs on
   port: '3000',
